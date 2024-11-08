3e95bb4cf63c92c492196bdabbea5e449129258d
Johan Rodahl Holmgren <itgsod.johan.holmgren@gmail.com>
2024-11-08 13:58:16 +0100

    init

---
+++ src/test/java/m7011e/the_homeric_odyssey/common_utils/CommonUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package m7011e.the_homeric_odyssey.common_utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
