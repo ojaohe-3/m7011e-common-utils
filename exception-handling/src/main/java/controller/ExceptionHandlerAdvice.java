39861df40633264fd654581d4cbdca225f73e7c6
Johan Rodahl Holmgren <itgsod.johan.holmgren@gmail.com>
2024-11-18 17:16:40 +0100

    Rename modules and fixed the aot problem

--- exception-handling/src/main/java/controller/ExceptionHandlerAdvice.java
+++ exception-handling/src/main/java/controller/ExceptionHandlerAdvice.java
@@ -1,5 +1,8 @@
 package controller;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 @ControllerAdvice
 public class ExceptionHandlerAdvice extends ResponseEntityExceptionHandler
 {
